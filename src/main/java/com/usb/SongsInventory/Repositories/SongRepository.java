2066a4ce97a1bcfcff03add54b3547bf97256c9b
NikoUSB <nalvarados@academia.usbbog.edu.co>
2025-05-05 12:09:43 -0500

    Update SongRepository

--- src/main/java/com/usb/SongsInventory/Repositories/SongRepository.java
+++ src/main/java/com/usb/SongsInventory/Repositories/SongRepository.java
@@ -1,4 +1,15 @@
 package com.usb.SongsInventory.Repositories;
 
-public interface SongRepository {
+import com.usb.SongsInventory.Entities.SongEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SongRepository extends JpaRepository<SongEntity, String> {
+
+    Page<SongEntity> findAllByGameNameContaining(String GameName, Pageable pageable);
+
+    @Override
+    Page<SongEntity> findAll(Pageable pageable);
+
 }
