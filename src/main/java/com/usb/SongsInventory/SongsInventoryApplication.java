2a47616225db00405def3513e060f9c659f27cc6
NikoUSB <nalvarados@academia.usbbog.edu.co>
2025-05-05 11:38:12 -0500

    Create proyect

---
+++ src/main/java/com/usb/SongsInventory/SongsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.usb.SongsInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsInventoryApplication.class, args);
+	}
+
+}
