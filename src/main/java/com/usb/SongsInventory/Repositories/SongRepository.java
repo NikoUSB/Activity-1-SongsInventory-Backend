b8e2b39cff4e127071318e37496c09b576029eca
NikoUSB <nalvarados@academia.usbbog.edu.co>
2025-05-05 12:24:22 -0500

    Update Song Repository and Service

--- src/main/java/com/usb/SongsInventory/Repositories/SongRepository.java
+++ src/main/java/com/usb/SongsInventory/Repositories/SongRepository.java
@@ -1,13 +1,16 @@
 package com.usb.SongsInventory.Repositories;
 
 import com.usb.SongsInventory.Entities.SongEntity;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SongRepository extends JpaRepository<SongEntity, String> {
 
-    Page<SongEntity> findAllByGameNameContaining(String GameName, Pageable pageable);
+    Page<SongEntity> findAllBySongNameContaining(String SongName, Pageable pageable);
 
     @Override
     Page<SongEntity> findAll(Pageable pageable);
--- src/main/java/com/usb/SongsInventory/Services/SongService.java
+++ src/main/java/com/usb/SongsInventory/Services/SongService.java
@@ -1,4 +1,90 @@
 package com.usb.SongsInventory.Services;
 
+import com.usb.SongsInventory.Entities.SongEntity;
+import com.usb.SongsInventory.Repositories.SongRepository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class SongService {
+
+    private final SongRepository songRepository;
+
+    public ResponseEntity<?> getAllSongs(Pageable pageable) {
+        Page<SongEntity> songs = songRepository.findAll(pageable);
+        return ResponseEntity.ok(songs);
+    }
+
+    public ResponseEntity<?> getSongById(String id) {
+        SongEntity song = songRepository.findById(id).orElse(null);
+        if (song == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(song);
+    }
+
+    public ResponseEntity<?> getSongsBySongName(String songName, Pageable pageable) {
+        Page<SongEntity> songs = songRepository.findAllBySongNameContaining(songName, pageable);
+        return ResponseEntity.ok(songs);
+    }
+
+    private ResponseEntity<?> getResponseEntity(Page<SongEntity> songs) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("TotalElements", songs.getTotalElements());
+        response.put("TotalPages", songs.getTotalPages());
+        response.put("CurrentPage", songs.getNumber());
+        response.put("NumberOfElements", songs.getNumberOfElements());
+        response.put("Songs", songs.getContent());
+        return ResponseEntity.ok(response);
+    }
+
+    //******//
+
+    public ResponseEntity<?> addSong(SongEntity songToAdd) {
+        Page<SongEntity> song = songRepository.findAllBySongNameContaining(
+                songToAdd.getName(),
+                Pageable.unpaged());
+        if (song.getTotalElements() > 0) {
+            return new ResponseEntity<>(Collections.singletonMap("Status", String.format("Song already exists with %d coincidences.", song.getTotalElements())), HttpStatus.CONFLICT);
+        } else {
+            SongEntity savedSong = songRepository.save(songToAdd);
+            return new ResponseEntity<>(Collections.singletonMap("Status", String.format("Added Song with ID %s", savedSong.getId())), HttpStatus.CREATED);
+        }
+    }
+
+    public ResponseEntity<?> updateSong(String id, SongEntity songToUpdate) {
+        Optional<SongEntity> song = songRepository.findById(id);
+        if (song.isEmpty()) {
+            return new ResponseEntity<>(Collections.singletonMap("Status", String.format("Song with ID %s not found.", id)), HttpStatus.NOT_FOUND);
+        }
+        SongEntity existingSong = song.get();
+
+        existingSong.setName(songToUpdate.getName());
+        existingSong.setArtist(songToUpdate.getArtist());
+        existingSong.setAlbum(songToUpdate.getAlbum());
+        existingSong.setYear(songToUpdate.getYear());
+        songRepository.save(existingSong);
+
+        return ResponseEntity.ok(Collections.singletonMap("Status", String.format("Updated Song with ID %s", existingSong.getId())));
+    }
+
+    public ResponseEntity<?> deleteSong(String id) {
+        Optional<SongEntity> song = songRepository.findById(id);
+        if (song.isEmpty()) {
+            return new ResponseEntity<>(Collections.singletonMap("Status", String.format("Song with ID %s doesn't exist.", id)),HttpStatus.NOT_FOUND);
+        }
+        songRepository.deleteById(id);
+        return ResponseEntity.ok(Collections.singletonMap("Status", String.format("Deleted Song with ID %s", id)));
+    }
 }
